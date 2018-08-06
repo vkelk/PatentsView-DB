8df48ad4b3ab24eb0e64edf67a9f68ddff862384
Vasko Kelkocev <pisimina@gmail.com>
2018-08-07 01:29:59 +0200

    application_parser application table

---
+++ Scripts/XMLParsers/app_parser.py
@@ -0,0 +1,311 @@
+import argparse
+import concurrent.futures as cf
+import logging
+import logging.config
+import os
+import re
+import requests
+# from shutil import move
+import sys
+import time
+import zipfile
+from lxml import etree, html
+
+from db_pgsql import Db_applications as Db
+import settings
+
+
+WORK_DIR = settings.APP_XMLDIR
+LOG_DIR = 'logs/'
+MAIN_URL = 'https://bulkdata.uspto.gov/data/patent/application/redbook/fulltext/2018/'
+
+
+def create_logger():
+    date = time.strftime('%Y-%m-%d')
+    log_file = LOG_DIR + 'tm_parser_' + str(date) + '.log'
+    logging.config.fileConfig('log.ini', defaults={'logfilename': log_file})
+    return logging.getLogger(__name__)
+
+
+def download_html(url):
+    print('Downloading %s' % url)
+    html_content = None
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/56.0.2924.87 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q = 0.9, image / webp, * / *;q = 0.8"}
+    try:
+        r = requests.get(url, headers=headers, allow_redirects=True)
+        if r.status_code != 200:
+            print('[%s] Downloading %s' % r.status_code, url)
+            print(r.headers)
+            print(r.cookies)
+        else:
+            html_content = r.content
+    except requests.exceptions.RequestException as err:
+        print('Failed to open url %s' % url)
+        html_content = None
+    finally:
+        if type(html_content) is bytes:
+            return html_content.decode()
+        else:
+            return html_content
+
+
+def get_text_or_none(element, item_name):
+    item = element.xpath(item_name)
+    if len(item) > 0:
+        return str(item[0])
+    else:
+        return None
+
+
+def print_children(element, level=0):
+    print('* Content of', element)
+    print(element.attrib)
+    l = []
+    for child in element:
+        tag = child.tag
+        print(tag)
+        text = get_text_or_none(child, 'text()')
+        if text is not None and text.strip() != '':
+            print('\t', text)
+        else:
+            l.append(child)
+    if level > 0:
+        try:
+            level = level - 1
+            for child in l:
+                print_children(child, level)
+        except Exception:
+            pass
+
+
+def clean_file(raw_file):
+    '''
+    Alters the raw patentsview data files to produce valid XML. 
+    Original is invalid because it lacks a root tag and has multiple xml version tags
+    :param raw_file: raw patentsview file
+    :return  null, creates list of files
+    '''
+    logger.info('Cleaning XML file')
+    if settings.APP_XMLDIR not in raw_file:
+            raw_file = os.path.join(settings.APP_XMLDIR, raw_file)
+    new_file = raw_file[:-4] + '_clean.xml'
+    try:
+        with open(raw_file, 'r+') as f:
+            with open(new_file, 'w') as new_f:
+                new_f.write('<?xml version="1.0" encoding="UTF-8"?>' + "\n")
+                new_f.write('<root>' + '\n')
+                for line in f.readlines():
+                    if not line.lstrip().startswith('<?xml') and not line.lstrip().startswith('<!DOCTYPE'):
+                        if line.lstrip().startswith('<root'):
+                            file_already_cleaned = True
+                            break
+                        new_f.write(line)
+                new_f.write('</root>')
+        if file_already_cleaned:
+            os.remove(new_file)
+        else:
+            os.replace(new_file, raw_file)
+        # move(new_file, raw_file)
+    except Exception:
+        logger.exception('message')
+
+
+def download_file(url):
+    zip_filename = os.path.join(settings.APP_XMLDIR, url.split('/')[-1])
+    xml_filename = zip_filename.replace('zip', 'xml')
+    if os.path.isfile(zip_filename) or os.path.isfile(xml_filename):
+        logger.debug('File already exists.')
+    else:
+        logger.info('Downloading %s', url)
+        # NOTE the stream=True parameter
+        # logger.debug('Getting zip from %s' % url)
+        start_time = time.time()
+        r = requests.get(url, stream=True)
+        with open(zip_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+        r.close()
+        logger.info('File %s downloaded in [%s sec].', zip_filename, time.time() - start_time)
+    if not os.path.isfile(xml_filename) and os.path.isfile(zip_filename):
+        try:
+            zip_ref = zipfile.ZipFile(zip_filename, 'r')
+            zip_ref.extractall(settings.APP_XMLDIR)
+            zip_ref.close()
+            clean_file(xml_filename)
+            os.remove(zip_filename)
+        except Exception:
+            logger.error('UNZIP ERROR. Deleting file %s' % zip_filename)
+            os.remove(zip_filename)
+    return xml_filename.split('/')[-1]
+
+
+def get_urls(main_url):
+    html_content = download_html(main_url)
+    html_tree = html.fromstring(html_content)
+    html_tree.make_links_absolute(main_url)
+    content = html_tree.xpath('//div[@class="container"]/table[2]//tr')
+    files = ()
+    for row in content:
+        a = row.xpath('td/a[contains(@href, ".zip")]')
+        if len(a) > 0:
+            apc = {'url': a[0].get('href'),
+                   'size': row.xpath('td[2]//text()')[0],
+                   'date_string': row.xpath('td[3]//text()')[0]}
+            files = (apc,) + files
+    if len(files) > 0:
+        logger.info('Found %s XML files online', len(files))
+        return files
+    return None
+
+
+def parse_file(filename, file_id):
+    dbc = Db()
+    if WORK_DIR not in filename:
+        filename = os.path.join(settings.APP_XMLDIR, filename)
+    with open(filename, 'rb') as inputfile:
+        file_start_time = time.time()
+        logger.info('Parsing file %s' % filename)
+        context = etree.iterparse(inputfile, events=('end',), tag='us-patent-application')
+        for event, case in context:
+            data_application = case.find('us-bibliographic-data-application')
+            app_ref = data_application.find('application-reference')
+            app_id = int(get_text_or_none(app_ref, 'document-id/doc-number/text()'))
+            app_id_db = dbc.app_id_get(app_id, file_id)
+            if app_id_db is not None:
+                logger.info('APP_id %s do not exists in database and will be inserted', app_id)
+                new_file_date = int(re.sub(r"\D", "", filename))
+                db_file_date = int(re.sub(r"\D", "", app_id_db['filename']))
+                print(new_file_date, db_file_date)
+                if new_file_date > db_file_date \
+                        or app_id_db['status'] is False \
+                        or (new_file_date >= db_file_date and args.parseall and args.force):
+                    for t in ('trademark_app_case_files', 'trademark_app_case_file_event_statements',
+                              'trademark_app_case_file_headers', 'trademark_app_case_file_owners',
+                              'trademark_app_case_file_statements', 'trademark_app_classifications',
+                              'trademark_app_correspondents', 'trademark_app_design_searches',
+                              'trademark_app_foreign_applications', 'trademark_app_international_registration',
+                              'trademark_app_madrid_history_events', 'trademark_app_madrid_international_filing_record',
+                              'trademark_app_prior_registration_applications', 'trademark_app_us_codes'):
+                        dbc.delete_serial(app_id, t)
+                    logger.info('Processing existing serial number %s', app_id)
+                    parse_case(case, app_id, file_id)
+            else:
+                logger.info('Processing new app_id %s', app_id)
+                parse_app(case, app_id, filename)
+            sys.exit()
+            case.clear()
+    dbc.file_update_status(file_id, 'finished')
+    os.remove(filename)
+    logger.info('Finished parsing file %s in [%s sec]', filename, time.time() - file_start_time)
+
+
+def parse_app(case, app_id, filename):
+    data_application = case.find('us-bibliographic-data-application')
+    app_ref = data_application.find('application-reference')
+    pub_ref = data_application.find('publication-reference')
+    claims_element_list = case.findall('claims/claim')
+    print_children(case, 5)
+
+    def parse_application():
+        dbc = Db()
+
+        pub_date = get_text_or_none(pub_ref, 'document-id/date/text()')
+        if pub_date[6:] != "00":
+            pub_date = pub_date[:4] + '-' + pub_date[4:6] + '-' + pub_date[6:]
+            year = pub_date[:4]
+        else:
+            pub_date = pub_date[:4] + '-' + pub_date[4:6] + '-' + '01'
+            year = pub_date[:4]
+
+        abstract_p_list = case.findall('abstract/p')
+        abstract = ''
+        for p in abstract_p_list:
+            abstract += get_text_or_none(p, 'text()')
+
+        application = {
+            'id': year + '/' + get_text_or_none(pub_ref, 'document-id/doc-number/text()'),
+            'type': app_ref.attrib['appl-type'],
+            'number': get_text_or_none(pub_ref, 'document-id/doc-number/text()'),
+            'app_id': app_id,
+            'country': get_text_or_none(app_ref, 'document-id/country/text()'),
+            'date': pub_date,
+            'abstract': abstract,
+            'title': get_text_or_none(case, 'us-bibliographic-data-application/invention-title/text()'),
+            'granted': None,
+            'num_claims': len(claims_element_list),
+            'filename': filename.split('\\')[-1],
+        }
+        print(application)
+    
+    dbc = Db()
+    start_time = time.time()
+
+    parse_application()
+
+    # with cf.ThreadPoolExecutor(max_workers=12) as executor:
+    #     executor.submit(parse_case_files)
+    #     executor.submit(parse_headers)
+    #     executor.submit(parse_statements)
+    #     executor.submit(parse_event_statements)
+    #     executor.submit(parse_prior_registration_applications)
+    #     executor.submit(parse_foreign_applications)
+    #     executor.submit(parse_classifications)
+    #     executor.submit(parse_correspondents)
+    #     executor.submit(parse_owners)
+    #     executor.submit(parse_design_searches)
+    #     executor.submit(parse_international_registration)
+    #     executor.submit(parse_madrid_international_filing_record)
+
+    # dbc.case_file_update_status(doc_id, 'true')
+    logger.info('Inserted application %s in [%s sec]', app_id, time.time() - start_time)
+
+
+def main_worker(file):
+    dbc = Db()
+    file_check = dbc.file_check(file)
+    if file_check is None:
+        xml_filename = download_file(file['url'])
+        if xml_filename is not None:
+            inserted_id = dbc.file_insert(file, xml_filename)
+            parse_file(xml_filename, inserted_id)
+    elif file_check['status'] in ['new', ''] or file_check['status'] is None:
+        logger.warning('File %s exists into database. Going to process again', file_check['filename'])
+        if not os.path.isfile(os.path.join(WORK_DIR, file_check['filename'])):
+            xml_filename = download_file(file['url'])
+        else:
+            xml_filename = file_check['filename']
+        if settings.APP_XMLDIR not in xml_filename:
+            xml_filename = os.path.join(settings.APP_XMLDIR, xml_filename)
+        clean_file(xml_filename)
+        parse_file(xml_filename, file_check['id'])
+    else:
+        logger.info('File %s is already inserted into database.', file_check['filename'])
+        if args.parse:
+            logger.info('Nothing to work. Exiting.')
+            sys.exit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Downloads, parses and inserts to database patent applications.')
+    parser.add_argument('--parse', help='Parses most recent data.', action="store_true")
+    parser.add_argument('--parseall', help='Parses all the data.', action="store_true")
+    parser.add_argument('--force', help='Forces to discard old data, use with --parseall command.', action="store_true")
+    args = parser.parse_args()
+    if args.parse or args.parseall:
+        os.makedirs(os.path.dirname(WORK_DIR), exist_ok=True)
+        os.makedirs(os.path.dirname(LOG_DIR), exist_ok=True)
+        logger = create_logger()
+        files_tuple = get_urls(MAIN_URL)
+        # single-thread test
+        for file in files_tuple:
+            main_worker(file)
+        sys.exit()
+        with cf.ThreadPoolExecutor(max_workers=4) as executor:
+            executor.map(main_worker, files_tuple)
+    else:
+        parser.print_help()
+        sys.exit()
---
+++ Scripts/XMLParsers/db_pgsql.py
@@ -0,0 +1,142 @@
+import logging
+import psycopg2
+import psycopg2.extras
+from psycopg2.extensions import AsIs
+from psycopg2.extras import execute_values
+import time
+
+import settings
+
+
+class Db_applications(object):
+    """
+    Database handler
+    """
+
+    __config = settings.config
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        try:
+            self.cnx = psycopg2.connect(**self.__config)
+            self.cur = self.cnx.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+            self.cur.execute("SET SEARCH_PATH = %s" % 'patent_app_v2')
+            self.cnx.commit()
+            # self.logger.info('Connected to database')
+        except psycopg2.Error as err:
+            self.logger.error(err)
+
+    def file_check(self, file):
+        zip_filename = file['url'].split('/')[-1]
+        xml_filename = zip_filename.replace('zip', 'xml')
+        q = "SELECT id, status, filename, date_string FROM file_info WHERE url = %s or filename = %s"
+        self.cur.execute(q, (file['url'], xml_filename))
+        return self.cur.fetchone()
+
+    def file_insert(self, file, xml_filename):
+        q = "INSERT INTO file_info (filename, filesize, url, date_string) VALUES (%s, %s, %s, %s) RETURNING id"
+        try:
+            start_time = time.time()
+            self.cur.execute(q, (xml_filename, file['size'], file['url'], file['date_string']))
+            last_row = self.cur.fetchone()
+            self.cnx.commit()
+            self.logger.debug('Inserted file_info in database [%s sec]', time.time() - start_time)
+            return last_row['id']
+        except psycopg2.Error as err:
+            self.logger.error(err)
+            self.cnx.rollback()
+        return None
+
+    def file_update_status(self, id, status):
+        q = "UPDATE file_info SET status = %s, modified = now() WHERE id = %s RETURNING id, status"
+        try:
+            self.cur.execute(q, (status, id))
+            rowcount = self.cur.rowcount
+            self.cnx.commit()
+            self.logger.debug('Updated status for file_info in database')
+            return rowcount
+        except psycopg2.Error as err:
+            self.logger.error(err)
+        return None
+
+    def app_id_get(self, app_id, file_id):
+        q = "SELECT app.id, app.date, app.filename, fi.status FROM application app \
+            JOIN file_info fi ON fi.filename = app.filename WHERE app_id = %s"
+        self.cur.execute(q, (app_id,))
+        return self.cur.fetchone()
+
+    def case_file_update_status(self, serial_number, status):
+        if serial_number is None or status is None:
+            logging.error('UPDATE ERROR: Missing serial_number or status')
+            return None
+        q = "UPDATE trademark_app_case_files SET status = %s, modified = now() WHERE serial_number = %s RETURNING id"
+        try:
+            self.cur.execute(q, (status, serial_number))
+            rowcount = self.cur.rowcount
+            self.cnx.commit()
+            self.logger.debug('Updated status for case_files in database')
+            return rowcount
+        except psycopg2.Error as err:
+            self.logger.error(err)
+        return None
+
+    def delete_serial(self, serial_number, table):
+        if serial_number is None or table is None:
+            logging.error('DELETE ERROR: Missing serial_number or table')
+            return None
+        q = 'DELETE FROM %s WHERE serial_number = %s'
+        start_time = time.time()
+        try:
+            q = self.cur.mogrify(q, (AsIs(table), serial_number))
+            self.cur.execute(q)
+            rowcount = self.cur.rowcount
+            self.cnx.commit()
+            self.logger.warning('Deleted serial_number %s from table %s [%s sec]', serial_number, table, time.time() - start_time)
+            return rowcount
+        except psycopg2.Error as err:
+            self.logger.error(err)
+            self.cnx.rollback()
+        return None
+
+    def insert_listdict(self, lst, table):
+        if len(lst) == 0 :
+            return None
+        keys = lst[0].keys()
+        columns = ', '.join(keys)
+        values = []
+        for d in lst:
+            values.append(list(d.values()))
+        start_time = time.time()
+        q = 'INSERT INTO {0} ({1}) values %s RETURNING id'.format(table, columns)
+        try:
+            execute_values(self.cur, q, values)
+            rowcount = self.cur.rowcount
+            # self.cur.execute(q)
+            self.cnx.commit()
+            self.logger.debug('Inserted %s rows in table %s [%s sec]', rowcount, table, time.time() - start_time)
+            return rowcount
+        except psycopg2.Error as err:
+            self.logger.error(err)
+            self.cnx.rollback()
+        return None
+
+    def insert_dict(self, d, table):
+        if d is None or table is None:
+            logging.error('INSERT ERROR: Missing dict or table')
+            return None
+        keys = d.keys()
+        columns = ', '.join(keys)
+        values = ', '.join(['%({})s'.format(k) for k in keys])
+        start_time = time.time()
+        q = 'INSERT INTO {0} ({1}) values ({2}) RETURNING id'.format(table, columns, values)
+        try:
+            q = self.cur.mogrify(q, d)
+            self.cur.execute(q)
+            self.cnx.commit()
+            last_row = self.cur.fetchone()
+            self.logger.debug('Inserted id [%s] in table %s [%s sec]', last_row['id'], table, time.time() - start_time)
+            return last_row['id']
+        except psycopg2.Error as err:
+            self.logger.error(err)
+            self.cnx.rollback()
+        return None
